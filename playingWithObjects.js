d0b5071cc867bf0a3a253111d160012d846bc35b
dylanPMurphy <dylanmurphydba@gmail.com>
2020-08-20 19:02:56 -0700

    Finished

--- playingWithObjects.js
+++ playingWithObjects.js
@@ -5,3 +5,10 @@
     How would you print/log the name and age of each user using a for loop?  Your output should look something like this
 */
 var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
+
+console.log(users[1].age);
+console.log(users[0].name);
+
+for(var i = 0; i<users.length; i++){
+    console.log(`${users[i].name} - ${users[i].age}`);
+}
