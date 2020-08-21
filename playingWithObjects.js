f870cc522f194eaa8b5b65b6203a9eda204be63f
dylanPMurphy <dylanmurphydba@gmail.com>
2020-08-20 18:44:34 -0700

    Init

---
+++ playingWithObjects.js
@@ -0,0 +1,7 @@
+
+/*
+    How would you print/log John's age?
+    How would you print/log the name of the first object?
+    How would you print/log the name and age of each user using a for loop?  Your output should look something like this
+*/
+var users = [{name: "Michael", age:37}, {name: "John", age:30}, {name: "David", age:27}];
